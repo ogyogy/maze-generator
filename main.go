2700c6f38c405f383756f20d9d8ddf1a72296823
ogyogy <47146269+ogyogy@users.noreply.github.com>
2022-12-01 15:25:31 +0000

    Refactoring

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import "fmt"
 
-// 迷路を高さheight * 幅widthマスで初期化
 // 0は壁、1は通路を表す
+const WALL = 0
+const PATH = 1
+
+// 迷路を高さheight * 幅widthマスで初期化
 func initMaze(height, width int) ([][]int, error) {
 	// 高さ、幅はそれぞれ5以上の奇数を指定する必要がある
 	if height < 5 || width < 5 {
@@ -11,7 +14,7 @@ func initMaze(height, width int) ([][]int, error) {
 		// スライスのゼロ値はnil
 		// nilスライスは0の長さと容量を持つ
 		return nil, fmt.Errorf("error: height and width must be at least 5")
-	} else if height%2 == 0 || width%2 == 0 {
+	} else if height%2 == WALL || width%2 == WALL {
 		return nil, fmt.Errorf("error: height and width must be odd numbers")
 	}
 	field := make([][]int, height)
@@ -28,7 +31,7 @@ func generateMaze(maze [][]int, x, y int) error {
 	if x < 1 || y < 1 || x%2 == 0 || y%2 == 0 {
 		return fmt.Errorf("error: x and y must be odd numbers greater than or equal to 1")
 	} else {
-		maze[y][x] = 1
+		maze[y][x] = PATH
 	}
 	return nil
 }
