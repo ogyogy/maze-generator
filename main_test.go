00baf3266c49e1e40365567b7117a9a193a9d08f
ogyogy <47146269+ogyogy@users.noreply.github.com>
2022-12-01 14:06:05 +0000

    Add TestInitField

---
+++ main_test.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestInitField(t *testing.T) {
+	fmt.Println(errors.Is(nil, nil))
+	type args struct {
+		height int
+		width  int
+	}
+	tests := []struct {
+		name        string
+		args        args
+		want_fileld [][]int
+		want_err    error
+	}{
+		{
+			// 正常系
+			name:        "positive",
+			args:        args{height: 5, width: 5},
+			want_fileld: [][]int{{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}},
+			want_err:    nil,
+		},
+		{
+			// 異常系 (高さが5未満)
+			name:        "negative",
+			args:        args{height: 4, width: 5},
+			want_fileld: nil,
+			want_err:    fmt.Errorf("error: height and width must be at least 5"),
+		},
+		{
+			// 異常系 (幅が5未満)
+			name:        "negative",
+			args:        args{height: 5, width: 4},
+			want_fileld: nil,
+			want_err:    fmt.Errorf("error: height and width must be at least 5"),
+		},
+		{
+			// 異常系 (高さ、幅ともに5未満)
+			name:        "negative",
+			args:        args{height: 4, width: 4},
+			want_fileld: nil,
+			want_err:    fmt.Errorf("error: height and width must be at least 5"),
+		},
+		{
+			// 異常系 (高さが偶数)
+			name:        "negative",
+			args:        args{height: 6, width: 5},
+			want_fileld: nil,
+			want_err:    fmt.Errorf("error: height and width must be odd numbers"),
+		},
+		{
+			// 異常系 (幅が偶数)
+			name:        "negative",
+			args:        args{height: 5, width: 6},
+			want_fileld: nil,
+			want_err:    fmt.Errorf("error: height and width must be odd numbers"),
+		},
+		{
+			// 異常系 (高さ、幅ともに偶数)
+			name:        "negative",
+			args:        args{height: 6, width: 6},
+			want_fileld: nil,
+			want_err:    fmt.Errorf("error: height and width must be odd numbers"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got_field, got_err := initField(tt.args.height, tt.args.width); !reflect.DeepEqual(got_field, tt.want_fileld) || (got_err != nil && got_err.Error() != tt.want_err.Error()) {
+				t.Errorf("field = %v, err = %v, want field = %v, err = %v, %v, %v", got_field, got_err, tt.want_fileld, tt.want_err, reflect.DeepEqual(got_field, tt.want_fileld), errors.Is(got_err, tt.want_err))
+			}
+		})
+	}
+}
